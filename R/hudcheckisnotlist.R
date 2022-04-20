c847f6c1283d6dcb21b46b9648cc4c0e8523ff45
Emmet Tam <etam4269@emmets-air.myfiosgateway.com>
2022-04-20 14:09:20 -0400

    do not eval new crosswalk a dataset vignette

--- R/hudcheckisnotlist.R
+++ R/hudcheckisnotlist.R
@@ -9,7 +9,7 @@ check_is_not_list <- function(dataframe) {
   if(typeof(dataframe) != "list") return(FALSE)
 
   for(i in seq_len(ncol(dataframe))) {
-    if(typeof(dataframe[[i]]) != "character" && typeof(dataframe[[i]]) != "integer" && typeof(dataframe[[i]]) != "numeric" && typeof(dataframe[[i]]) != "double") {
+    if(typeof(dataframe[,i]) != "character" && typeof(dataframe[,i]) != "integer" && typeof(dataframe[,i]) != "numeric" && typeof(dataframe[,i]) != "double") {
       return(FALSE)
     }
   }
